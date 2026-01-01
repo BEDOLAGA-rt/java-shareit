258e36550212bf761d97cbf0735b0ec4205c7cc4
arsenijprokofev <arseniyprokofev17279@gmail.com>
2026-01-01 19:25:44 +0300

    2 версия

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -4,9 +4,15 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto create(Long userId, ItemDto dto);
+
     ItemDto update(Long userId, Long itemId, ItemDto dto);
+
     ItemDto getById(Long itemId);
+
     List<ItemDto> getByOwner(Long userId);
+
     List<ItemDto> search(String text);
+
 }
