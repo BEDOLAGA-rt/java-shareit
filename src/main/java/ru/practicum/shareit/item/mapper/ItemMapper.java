32127ad19e59f9004be4512a0107d33d88daca4b
arsenijprokofev <arseniyprokofev17279@gmail.com>
2026-01-08 23:29:57 +0300

    2 версия

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -6,21 +6,21 @@ import ru.practicum.shareit.item.model.Item;
 public class ItemMapper {
 
     public static Item toItem(ItemDto dto, Long ownerId) {
-        return new Item(
-                dto.getId(),
-                dto.getName(),
-                dto.getDescription(),
-                dto.getAvailable(),
-                ownerId
-        );
+        Item item = new Item();
+        item.setId(dto.getId());
+        item.setName(dto.getName());
+        item.setDescription(dto.getDescription());
+        item.setAvailable(dto.getAvailable());
+        item.setOwnerId(ownerId);
+        return item;
     }
 
     public static ItemDto toDto(Item item) {
-        return new ItemDto(
-                item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.getAvailable()
-        );
+        ItemDto dto = new ItemDto();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setDescription(item.getDescription());
+        dto.setAvailable(item.getAvailable());
+        return dto;
     }
 }
