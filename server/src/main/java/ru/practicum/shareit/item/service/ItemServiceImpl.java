3446465e38ad71c3f5ff2fcfad7c1193a0afcf34
arsenijprokofev <arseniyprokofev17279@gmail.com>
2026-01-10 04:02:06 +0300

    vv

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -35,10 +35,6 @@ import static java.time.LocalDateTime.*;
 import static java.util.Collections.*;
 import static java.util.Comparator.*;
 
-/**
- * @author Oleg Khilko
- */
-
 @Slf4j
 @Service
 @AllArgsConstructor
